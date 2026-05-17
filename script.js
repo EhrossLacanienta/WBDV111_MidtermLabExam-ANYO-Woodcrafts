c39e4b970437753d396363410cd71ebeb8b82cd5
EhrossLacanienta <ealacanienta1910ant@student.fatima.edu.ph>
2026-05-18 01:10:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-// NAVBAR TOGGLE
+// ===== NAVBAR TOGGLE =====
 const toggle = document.getElementById("menu-toggle");
 const nav = document.getElementById("nav-links");
 
 if (toggle && nav) {
 
   toggle.addEventListener("click", () => {
+
     nav.classList.toggle("active");
 
-    // ADD THIS: change icon ☰ ↔ ✖
-    if (nav.classList.contains("active")) {
-      toggle.innerHTML = "✖";
-    } else {
-      toggle.innerHTML = "☰";
-    }
+    toggle.textContent =
+      nav.classList.contains("active") ? "✖" : "☰";
+
   });
 
-  // AUTO CLOSE MENU
+  // ===== AUTO CLOSE MENU =====
   const links = document.querySelectorAll(".nav-links a");
 
   links.forEach(link => {
-    link.addEventListener("click", () => {
-      nav.classList.remove("active");
 
-      // reset icon back to hamburger
-      toggle.innerHTML = "☰";
+    link.addEventListener("click", () => {
+
+      nav.classList.remove("active");
+      toggle.textContent = "☰";
+
     });
+
   });
 
 }
